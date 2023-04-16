65b42ebed3e2eb620bc455656bec273c556d06db
beloveddd <yuliya.bialova@gmail.com>
2023-04-16 20:44:19 +0300

    feat: create PersistenceService for working with LS

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { RegisterComponent } from './components/register/register.component';
 import { reducer } from './store/reducers';
 import { AuthService } from './services/auth.service';
-import { EffectsModule } from '@ngrx/effects';
 import { RegisterEffect } from './store/effects/register.effect';
 import { BackendErrorMessagesModule } from '../shared/modules/backendErrorMessages/backendErrorMessages.module';
+import { PersistenceService } from '../shared/services/persistence.service';
 
 const routes: Routes = [
   {
@@ -28,6 +29,6 @@ const routes: Routes = [
     EffectsModule.forFeature([RegisterEffect]),
   ],
   declarations: [RegisterComponent],
-  providers: [AuthService],
+  providers: [AuthService, PersistenceService],
 })
 export class AuthModule {}
--- src/app/shared/modules/backendErrorMessages/components/backendErrorsMessages.component.ts
+++ src/app/shared/modules/backendErrorMessages/components/backendErrorsMessages.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { BackendErrorsInterface } from '../../../types/backendErrors.interface';
 
 @Component({
